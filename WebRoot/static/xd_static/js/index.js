24142ee191ce087e4a03d1f4048546addfcae4ba
guoyansi <1012447978@qq.com>
2015-07-31 07:56:38 +0800

    小测试

--- WebRoot/static/xd_static/js/index.js
+++ WebRoot/static/xd_static/js/index.js
@@ -1,10 +1,10 @@
 var homeModule=angular.module("xdModule",[]);
 homeModule.controller("indexController",function($scope){
 	$scope.clickMenuItem=function(i){
-		$(".xd-menu-box").find("li").removeClass("xd-menu-item-hover");
+		/*$(".xd-menu-box").find("li").removeClass("xd-menu-item-hover");
 		$(".xd-menu-box").find("li").eq(i).addClass("xd-menu-item-hover");
 		var rel=$(".xd-menu-box .xd-menu-item-hover .xd-menu-text").attr("rel");
-		$scope.src=rel;
-		
+		$scope.src=rel;*/
+		$scope.boolean=true;
 	}
 });
